4d7e38a634fe38f7dccba13687c4d84ee91e9e46
PastaMage <279487@student.pwr.edu.pl>
2024-12-02 19:27:24 +0100

    AdventOfCode Day One

--- AdventOfCodeOne/Program.cs
+++ AdventOfCodeOne/Program.cs
@@ -6,22 +6,25 @@ namespace AdventOfCodeOne
     {
         private static void Main(string[] args)
         {
-            const string fileName = "/home/pastamage/advent_of_code/AdventOfCodeOne/AdventOfCodeOne/input.txt"; 
+            const string fileName = "C:\\Users\\max54\\RiderProjects\\advent_of_code\\AdventOfCodeOne\\input.txt"; 
             string input = System.IO.File.ReadAllText(fileName); 
             string[] lines = input.Split('\n'); 
             int[] numbersOne = new int[lines.Length]; 
             int[] numbersTwo = new int[lines.Length]; 
-            for(int i = 0; i < lines.Length; i++) 
-            { 
+            for(int i = 0; i < lines.Length; i++)
+            {
                 int[] nums = lines[i].Split("   ").Select(int.Parse).ToArray(); 
                 numbersOne[i] = nums[0]; 
                 numbersTwo[i] = nums[1]; 
             }
-            
-            foreach (int line in numbersOne) 
+            Array.Sort(numbersOne);
+            Array.Sort(numbersTwo);
+            int result = 0;
+            for(int i = 0; i < numbersOne.Length; i++) 
             { 
-                Console.WriteLine(line); 
+                result += Math.Abs(numbersOne[i] - numbersTwo[i]);
             }
+            Console.WriteLine(result);
         }
     }
 }
