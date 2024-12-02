7e7fb3dd49e92e67c47a965a0583e9f60610052d
PastaMage <279487@student.pwr.edu.pl>
2024-12-02 14:02:18 +0100

    Start

---
+++ AdventOfCodeOne/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AdventOfCodeOne 
+{ 
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            const string fileName = "/home/pastamage/advent_of_code/AdventOfCodeOne/AdventOfCodeOne/input.txt"; 
+            string input = System.IO.File.ReadAllText(fileName); 
+            string[] lines = input.Split('\n'); 
+            int[] numbersOne = new int[lines.Length]; 
+            int[] numbersTwo = new int[lines.Length]; 
+            for(int i = 0; i < lines.Length; i++) 
+            { 
+                int[] nums = lines[i].Split("   ").Select(int.Parse).ToArray(); 
+                numbersOne[i] = nums[0]; 
+                numbersTwo[i] = nums[1]; 
+            }
+            
+            foreach (int line in numbersOne) 
+            { 
+                Console.WriteLine(line); 
+            }
+        }
+    }
+}
